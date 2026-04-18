2086092d5813d683db51c2319d4428125ce168a8
Fatih Arslan <arslanfatih929@gmail.com>
2026-04-18 21:48:46 +0300

    Güvenlik: Hassas veriler içeren debug dosyaları kaldırıldı ve .gitignore güncellendi

--- tmp-wsdl.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const res = await fetch("https://www.rsrivashoes.com/Servis/UrunServis.svc?wsdl");
-    const text = await res.text();
-    const ops = text.match(/<wsdl:operation name="([^"]+)"/g) || [];
-    const names = ops.map(o => o.replace('<wsdl:operation name="', '').replace('"', ''));
-    console.log("Operations:", Array.from(new Set(names)));
-})();
